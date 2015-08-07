58013d72d9ff0999835b1f262d65cd189c164776
hygeng <hygeng@jointwisdom.cn>
2015-08-07 16:37:06 +0800

    1

---
+++ CIProjectDemo1/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIProjectDemo1
+{
+    public class Main
+    {
+        public int PrintNumberSquare(int num)
+        {
+            return num*num;
+        }
+
+        public string PrintString(string str)
+        {
+            return string.Format("现在输出的内容是：{0}", str);
+        }
+    }
+}
